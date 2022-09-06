57bc16c48a349d003d40d615aa2e2db1ff3d1ff8
bazirake <bazirakeric1@gmail.com>
2022-09-06 18:21:42 +0200

    changess

--- index.php
+++ index.php
@@ -625,25 +625,6 @@
     <!-- ============================================================== -->
     <!-- All Jquery -->
     <!-- ============================================================== -->
-    <script src="assets/plugins/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="assets/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="html/js/app-style-switcher.js"></script>
-    <!--Wave Effects -->
-    <script src="html/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="html/js/sidebarmenu.js"></script>
-    <!-- ============================================================== -->
-    <!-- This page plugins -->
-    <!-- ============================================================== -->
-    <!-- chartist chart -->
-    <script src="assets/plugins/chartist-js/dist/chartist.min.js"></script>
-    <script src="assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
-    <!--c3 JavaScript -->
-    <script src="assets/plugins/d3/d3.min.js"></script>
-    <script src="assets/plugins/c3-master/c3.min.js"></script>
-    <!--Custom JavaScript -->
-    <script src="html/js/pages/dashboards/dashboard1.js"></script>
-    <script src="html/js/custom.js"></script>
+   <?php include('include/script.php');?>
 </body>
 </html>
